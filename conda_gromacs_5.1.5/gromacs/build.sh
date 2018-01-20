641bff4ca86a1b07ad455a51b9497355000749ed
Yi Hsiao <hsiaoyi0504@gmail.com>
2018-01-20 09:29:54 -0500

    fix

--- conda_gromacs_5.1.5/gromacs/build.sh
+++ conda_gromacs_5.1.5/gromacs/build.sh
@@ -4,7 +4,7 @@ cd $SRC_DIR/gromacs_5.1.5/build
 cmake -DGMX_MPI=OFF \
       -DCMAKE_C_COMPILER=${PREFIX}/bin/x86_64-conda_cos6-linux-gnu-gcc \
       -DCMAKE_CXX_COMPILER=${PREFIX}/bin/x86_64-conda_cos6-linux-gnu-g++ \
-      -DCMAKE_INSTALL_PREFIX=$(pwd) ..
+      -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 
 make
 make install
