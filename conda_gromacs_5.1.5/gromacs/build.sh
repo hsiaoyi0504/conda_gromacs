80b7000c7215e8d3f3e2270e031179fafe195b0a
Yi Hsiao <hsiaoyi0504@gmail.com>
2018-01-28 09:26:29 -0500

    update

--- conda_gromacs_5.1.5/gromacs/build.sh
+++ conda_gromacs_5.1.5/gromacs/build.sh
@@ -1,11 +1,8 @@
 mkdir $SRC_DIR/gromacs_5.1.5/build
 cd $SRC_DIR/gromacs_5.1.5/build
 
-cmake -DGMX_X11=OFF \
-      -DGMX_CXX11=OFF \
-      -DGMX_MPI=OFF \
-      -DGMX_BUILD_OWN_FFTW=ON \
-      -DGMX_EXTERNAL_BOOST=OFF \
+cmake -DGMX_MPI=OFF \
+      -DGMX_BUILD_OWN_FFTW=OFF \
       -DCMAKE_C_COMPILER=${PREFIX}/bin/x86_64-conda_cos6-linux-gnu-gcc \
       -DCMAKE_CXX_COMPILER=${PREFIX}/bin/x86_64-conda_cos6-linux-gnu-g++ \
       -DGMX_PREFER_STATIC_LIBS=ON \
